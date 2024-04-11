65517b36ecf0e5e187c8740b08d187b29d43ec54
imronadil <imronadil@yahoo.com>
2024-04-11 12:56:13 +0700

    name change

--- src/pages/3/index.tsx
+++ src/pages/3/index.tsx
@@ -18,7 +18,7 @@ const ThirdPage = () => {
 				
 				<div>
 					<PersonalDetail
-						name="Imron Adi"
+						name="Imron Adi (Dio)"
 						subtitle="The eldest son of Mr. Suyamta and Mrs. Guslinawati"
 						instagram="https://instagram.com/imronadil"
 					/>
@@ -26,7 +26,7 @@ const ThirdPage = () => {
 					<h1 className="responsive">&</h1>
 					<br />
 					<PersonalDetail
-						name="Okta Anggra"
+						name="Okta Anggra (Reni)"
 						subtitle="The youngest daughter of Mr. Muhammad Mujib and Mrs. Wiwik Khayati"
 						instagram="https://instagram.com/okta.anggra"
 					/>
