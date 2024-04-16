b599d7ab217f913b6b88086f92cf7de55b268ac5
FaishalJitera <faishal.rahman@jitera.com>
2024-04-16 09:08:54 +0700

    add code to change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,36 @@ import "./globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Imron & Anggra Invitation",
-  description: "Invitation for Imron & Anggra's wedding!",
+	title: "Imron & Anggra Invitation",
+	description: "Invitation for Imron & Anggra's wedding!",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<link rel="shortcut icon" href="/images/favicon.ico" />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/images/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/images/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/images/favicon-16x16.png"
+			/>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
